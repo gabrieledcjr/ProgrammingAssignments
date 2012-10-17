3ba7f83f878b0205bdd67cc1b0bd2c3e5724b4e6
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2012-10-17 12:59:39 -0700

    formatting changes

--- yahtzee_pa5/yahtzee_pa5/main.c
+++ yahtzee_pa5/yahtzee_pa5/main.c
@@ -1,9 +1,9 @@
 /*************************************************************************
  *     XX  XX    XX    XX  XX  XXXXXX  XXXXXX  XXXXXX  XXXXXX  XX        *
  *     XX  XX  XX  XX  XX  XX    XX        X   XX      XX      XX        *
- *	    XXXX   XXXXXX  XXXXXX    XX      XX    XXXX    XXXX    XX        * 
- *	     XX    XX  XX  XX  XX    XX     X      XX      XX                *
- *	     XX    XX  XX  XX  XX    XX    XXXXXX  XXXXXX  XXXXXX  XX        * 
+ *      XXXX   XXXXXX  XXXXXX    XX      XX    XXXX    XXXX    XX        * 
+ *       XX    XX  XX  XX  XX    XX     X      XX      XX                *
+ *       XX    XX  XX  XX  XX    XX    XXXXXX  XXXXXX  XXXXXX  XX        * 
  * Filename: main.c                                                      *
  * Programmer: Gabriel V. de a Cruz Jr.                                  *
  * Class: CptS 121, Fall 2012 ; Lab Section 7                            *
